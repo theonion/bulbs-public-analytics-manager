1b8e3b78ab3ca0c946e0f140d27e3e2170ebed60
Andrew Kos <akos@theonion.com>
2015-10-01 15:49:00 -0500

    Add error message for missing ingest url

--- src/analytics-ingest/analytics-ingest.js
+++ src/analytics-ingest/analytics-ingest.js
@@ -1,8 +1,23 @@
-module.exports = {
+var _AnalyticsIngestError = function (message) {
+  this.name = 'AnalyticsIngestError';
+  this.message = message || '';
+
+  var error = new Error(this.message);
+  error.name = this.name;
+  this.stack = error.stack;
+};
+AnalyticsManagerError.prototype = Object.create(Error.prototype);
+
+var AnalyticsIngest = {
   /**
    * Sends the page information to the ingestion endpoint
    */
   sendEvent: function (endpoint) {
+
+    if (typeof(endpoint) !== 'string') {
+      throw new _AnalyticsIngestError('Ingest endpoint must be set!');
+    }
+
     var cacheBuster = (new Date()).getTime();
 
     var url =
@@ -18,3 +33,5 @@ module.exports = {
     });
   }
 };
+
+module.exports = AnalyticsIngest;
